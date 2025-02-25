aea1455864d6146ace24858554a0e98d0bc05b06
IliaAzhel <aje-ilia2013@yandex.by>
2025-02-25 09:53:34 +0300

    add env and network settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+const envconfig = require('dotenv')
+const { ETHERSCAN_API_KEY, PK } = envconfig.parsed || {};
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
 };
 
-export default config;
+export default {
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io`,
+      chainId: 1,
+      accounts: [`0x${PK || '1000000000000000000000000000000000000000000000000000000000000000'}`],
+    },
+  },
+  solidity: config.solidity,
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: `${ETHERSCAN_API_KEY}`,
+    customChains: []
+  },
+}
